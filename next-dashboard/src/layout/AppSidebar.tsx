e6a1e5c2382d8e1ed94f0311867de71fdcc91833
kahmunrah <jiminzeo@gmail.com>
2025-08-18 14:14:58 +1200

    feat: added custom Align logo and sidebar updates

--- next-dashboard/src/layout/AppSidebar.tsx
+++ next-dashboard/src/layout/AppSidebar.tsx
@@ -313,14 +313,14 @@ const AppSidebar: React.FC = () => {
             <>
               <Image
                 className="dark:hidden"
-                src="/images/logo/logo.svg"
+                src="/images/logo/Align-Logo-with-words.svg"
                 alt="Logo"
                 width={150}
                 height={40}
               />
               <Image
                 className="hidden dark:block"
-                src="/images/logo/logo-dark.svg"
+                src="/images/logo/Align-Logo-with-words.svg"
                 alt="Logo"
                 width={150}
                 height={40}
@@ -328,7 +328,7 @@ const AppSidebar: React.FC = () => {
             </>
           ) : (
             <Image
-              src="/images/logo/logo-icon.svg"
+              src="/images/logo/Align-logo-wordless-Default.svg"
               alt="Logo"
               width={32}
               height={32}
